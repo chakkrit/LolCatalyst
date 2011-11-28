215369fb2ba6a224fff7e986ce36b559614a4c08
Chakkrit Sripariwut <chakkrit@abctech-thailand.com>
2011-11-28 17:53:08 +0700

    Add authentication to project

--- lib/LolCatalyst/Lite.pm
+++ lib/LolCatalyst/Lite.pm
@@ -20,7 +20,12 @@ use Catalyst qw/
     -Debug
     ConfigLoader
     Static::Simple
-/;
+
+               Authentication
+               Authorization::Roles
+               Session
+               Session::State::Cookie
+               Session::Store::FastMmap /;
 
 extends 'Catalyst';
 
@@ -43,7 +48,25 @@ __PACKAGE__->config(
 );
 
 # Start the application
-__PACKAGE__->setup();
+__PACKAGE__->config( authentication => {
+    'default_realm' => 'users',
+    'realms' => {
+        'users' => {
+            'store' => {
+                'role_column' => 'role_text',
+                'user_class' => 'Auth::User',
+                'class' => 'DBIx::Class',
+            },
+            'credential' => {
+                 'password_type' => 'hashed',
+                 'password_field' => 'password',
+                 'password_hash_type' => 'SHA-1',
+                 'class' => 'HTTP',
+                 'type' => 'basic',
+             }
+        }
+    },
+});__PACKAGE__->setup();
 
 
 =head1 NAME
---
+++ t/model_Auth.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'LolCatalyst::Lite::Model::Auth' }
+
+done_testing();
