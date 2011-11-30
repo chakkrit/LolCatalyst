0014d2853928bbfc2e501b9bcc61e03977f92f2b
Chakkrit Sripariwut <chakkrit@abctech-thailand.com>
2011-11-30 08:53:29 +0700

    add POD to Authentication Schema class

--- lib/LolCatalyst/Lite/Auth/Schema.pm
+++ lib/LolCatalyst/Lite/Auth/Schema.pm
@@ -11,6 +11,20 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
+=head1 NAME
+
+LolCatalyst::Lite::Auth::Schema - Auth DBIC Schema Model
+
+=head1 AUTHOR
+
+chakkrit
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 # Created by DBIx::Class::Schema::Loader v0.07012 @ 2011-11-28 17:43:07
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:hW8oWpYBFBbyPM3ZTaYK/Q
